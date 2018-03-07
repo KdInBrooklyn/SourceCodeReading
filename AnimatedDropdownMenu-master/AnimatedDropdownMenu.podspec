dd80f8c0b6a2f798f1340152337ccf597a72e4c8
LiSen <lisen@LiSens-Mac-mini.local>
2018-03-07 10:32:06 +0800

    添加两个源码文件: 一个是实现带有抖动效果的cell重排功能(XWDragCellCollectionView); 另一个是使用Swift实现下拉菜单功能(AnimatedDropdownMenu)

---
+++ AnimatedDropdownMenu-master/AnimatedDropdownMenu.podspec
@@ -0,0 +1,18 @@
+Pod::Spec.new do |s|
+  s.name = 'AnimatedDropdownMenu'
+  s.version = '0.3.3'
+  s.summary = 'A clean interface dropdown menu used on iOS.'
+  s.description      = <<-DESC
+                       AnimatedDropdownMenu is a clean interface dropdown menu, appears underneath navigation bar to display a list of related items when you click on the navigation title.
+                       DESC
+  s.homepage = 'https://github.com/JonyFang/AnimatedDropdownMenu'
+  s.license  = 'MIT'
+  s.authors = { 'JonyFang' => 'jony.chunfang@gmail.com' }
+  s.source = { :git => 'https://github.com/JonyFang/AnimatedDropdownMenu.git', :tag => s.version.to_s }
+  
+  s.platform = :ios, '8.0'
+  s.requires_arc = true
+
+  s.source_files = 'Source/*.swift'
+  s.resources = 'Source/*.bundle'
+end
---
+++ AnimatedDropdownMenu-master/Examples/Examples/Extensions/UIColor+DropdownMenu.swift
@@ -0,0 +1,41 @@
+//
+//  UIColor+DropdownMenu.swift
+//  AnimatedDropdownMenu
+//
+//  Created by JonyFang on 17/3/1.
+//  Copyright © 2017年 JonyFang. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func menuLightGrayColor() -> UIColor {
+        return UIColor(red: 95/255.0, green: 105/255.0, blue: 117/255.0, alpha: 1.0)
+    }
+    
+    class func menuDarkGrayColor() -> UIColor {
+        return UIColor(red: 49/255.0, green: 59/255.0, blue: 71/255.0, alpha: 1.0)
+    }
+    
+    class func menuLightTextColor() -> UIColor {
+        return UIColor(red: 245/255.0, green: 245/255.0, blue: 245/255.0, alpha: 1.0)
+    }
+    
+    class func menuLightRedColor() -> UIColor {
+        return UIColor(red: 216/255.0, green: 85/255.0, blue: 96/255.0, alpha: 1.0)
+    }
+    
+    class func menuDarkRedColor() -> UIColor {
+        return UIColor(red: 160/255.0, green: 61/255.0, blue: 73/255.0, alpha: 1.0)
+    }
+    
+    class func menuPurpleColor() -> UIColor {
+        return UIColor(red: 143/255.0, green: 69/255.0, blue: 163/255.0, alpha: 1.0)
+    }
+    
+    class func menuGreenColor() -> UIColor {
+        return UIColor(red: 0, green: 162/255.0, blue: 138/255.0, alpha: 1.0)
+    }
+    
+}
