828410269ef51954638155ce9ba945d469c53d2c
LiSen <lisen@LiSens-Mac-mini.local>
2018-03-07 17:42:42 +0800

    添加对YYAppDelegate.m和YYRootViewController.m文件的阅读注释

--- YYKit-master/Demo/YYKitDemo/YYAppDelegate.m
+++ YYKit-master/Demo/YYKitDemo/YYAppDelegate.m
@@ -10,6 +10,7 @@
 #import "YYRootViewController.h"
 
 /// Fix the navigation bar height when hide status bar.
+#pragma mark YYExampleNavBar
 @interface YYExampleNavBar : UINavigationBar
 @end
 
@@ -36,6 +37,7 @@
 
 @end
 
+#pragma mark YYExampleNavController
 @interface YYExampleNavController : UINavigationController
 @end
 @implementation YYExampleNavController
@@ -55,17 +57,28 @@
 
 
 
-@implementation YYAppDelegate
+@implementation YYAppDelegate {
+    UIViewController *_secondController;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+#pragma mark new和alloc/init的区别
+/**
+ new:
+ 内部调用的是alloc/init方法, 但是使用new进行对象初始化时并不添加其他参数
+ */
     YYRootViewController *root = [YYRootViewController new];
+#pragma mark 使用自定义的UINavigationBar初始化一个UINavigationController实例对象, 然后调用实例方法push到根视图上
+//平时的做法就是直接使用rootViewController进行初始化, 然后将navigationBar属性赋值给UINavigationController实例对象
     YYExampleNavController *nav = [[YYExampleNavController alloc] initWithNavigationBarClass:[YYExampleNavBar class] toolbarClass:[UIToolbar class]];
     if ([nav respondsToSelector:@selector(setAutomaticallyAdjustsScrollViewInsets:)]) {
         nav.automaticallyAdjustsScrollViewInsets = NO;
     }
     [nav pushViewController:root animated:NO];
     
+#pragma mark 正常的UIWindow配置
     self.rootViewController = nav;
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = self.rootViewController;
     self.window.backgroundColor = [UIColor grayColor];
--- YYKit-master/Demo/YYKitDemo/YYRootViewController.m
+++ YYKit-master/Demo/YYKitDemo/YYRootViewController.m
@@ -19,6 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"YYKit Example";
+    //可变数组的mutableCopy是对内容进行复制
     self.titles = @[].mutableCopy;
     self.classNames = @[].mutableCopy;
     [self addCell:@"Model" class:@"YYModelExample"];
@@ -28,9 +29,10 @@
     [self addCell:@"Feed List Demo" class:@"YYFeedListExample"];
     [self.tableView reloadData];
     
-    //[self log];
+//    [self log];
 }
 
+#warning 查看作者对UIDevice相关扩展的实现
 - (void)log {
     printf("all:%.2f MB   used:%.2f MB   free:%.2f MB   active:%.2f MB  inactive:%.2f MB  wird:%.2f MB  purgable:%.2f MB\n",
            [UIDevice currentDevice].memoryTotal / 1024.0 / 1024.0,
@@ -40,6 +42,7 @@
            [UIDevice currentDevice].memoryInactive / 1024.0 / 1024.0,
            [UIDevice currentDevice].memoryWired / 1024.0 / 1024.0,
            [UIDevice currentDevice].memoryPurgable / 1024.0 / 1024.0);
+#pragma mark 该方法会一直循环被调用, 相比创建一个计时器来进行打印是个便捷的方法
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self log];
     });
@@ -67,6 +70,7 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     NSString *className = self.classNames[indexPath.row];
+#pragma mark 使用NSClassFromString来通过类名获取相关的类
     Class class = NSClassFromString(className);
     if (class) {
         UIViewController *ctrl = class.new;
